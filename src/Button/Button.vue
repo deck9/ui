490c9109952128a2be6c26e41e03b1ea80304389
Philipp Reinking <philipp@deck9.co>
2021-08-26 21:38:21 +0200

    add hover and focus states for button

--- src/Button/Button.vue
+++ src/Button/Button.vue
@@ -3,7 +3,7 @@
     class="relative inline-flex items-center focus:outline-none focus:ring active:ring transition duration-150 ease-in-out"
     type="button"
     :aria-label="label"
-    :class="classes"
+    :class="buttonClasses"
     @click="onClick"
   >
     <div :class="!isLoading || 'invisible'">
@@ -57,44 +57,61 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   isDisabled: false,
 });
 
-const classes = computed(() => {
-  return {
-    // colors
-    "text-white bg-blue-600 border-transparent hover:bg-blue-500 active:bg-blue-700 ring-blue-300":
+const buttonClasses = computed(() => {
+  const colors = {
+    "text-white bg-blue-600 border-transparent hover:bg-blue-700 active:bg-blue-700 ring-blue-800":
       props.type === "primary",
-    "text-blue-600 bg-white border-transparent hover:bg-grey-100 active:bg-grey-100 ring-blue-200":
+    "text-blue-600 bg-white border-transparent hover:bg-blue-100 hover:text-blue-700 active:bg-grey-100 ring-blue-300":
       props.type === "light",
-    "text-grey-50 bg-grey-700 border-transparent hover:bg-grey-900 active:bg-grey-900 ring-grey-300":
+    "text-grey-50 bg-grey-700 border-transparent hover:bg-grey-800 active:bg-grey-900 ring-grey-300 ring-opacity-50":
       props.type === "dark",
+  };
 
+  const ux = {
     "pointer-events-none opacity-75 cursor-not-allowed": props.isDisabled,
+  };
 
-    // sizes
-    "px-4 py-1 text-xs leading-5 font-normal rounded-lg":
-      props.size === "small",
-    "px-5 py-2 text-sm leading-5 font-medium rounded-lg":
+  const sizes = {
+    "px-4 py-1 text-xs leading-5 mono-100 rounded-lg": props.size === "small",
+    "px-5 py-2 text-sm leading-4 font-medium rounded-lg":
       props.size === "medium",
-    "px-12 py-5 text-lg leading-5 font-medium rounded-lg":
+    "px-8 py-3 text-base leading-6 font-medium rounded-lg":
       props.size === "large",
+  };
+
+  return {
+    ...colors,
+    ...ux,
+    ...sizes,
   };
 });
 
 const iconClasses = computed(() => {
-  return {
-    "ml-2":
+  const iconLeft = {
+    "mr-2 -ml-1":
+      ["small", "medium"].includes(props.size) && props.iconPosition === "left",
+    "mr-4 -ml-2":
+      ["large"].includes(props.size) && props.iconPosition === "left",
+  };
+
+  const iconRight = {
+    "ml-3 -mr-1":
       ["small", "medium"].includes(props.size) &&
       props.iconPosition === "right",
-    "ml-4 -mr-5 text-base":
+    "ml-3 -mr-3":
       ["large"].includes(props.size) && props.iconPosition === "right",
+  };
 
-    "mr-2":
-      ["small", "medium"].includes(props.size) && props.iconPosition === "left",
-    "mr-4 -ml-5 text-base":
-      ["large"].includes(props.size) && props.iconPosition === "left",
-
+  const colors = {
     "text-white": props.type === "primary",
-    "text-grey-700": props.type === "light",
+    "text-blue-600": props.type === "light",
     "text-grey-50": props.type === "dark",
+  };
+
+  return {
+    ...iconLeft,
+    ...iconRight,
+    ...colors,
   };
 });
 
--- src/Checkbox/Checkbox.vue
+++ src/Checkbox/Checkbox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center h-5">
     <input
-      class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+      class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-grey-300 rounded"
       type="checkbox"
     />
   </div>
