0d0e5852908fb3e0f489444b5f323a3097e865e8
Philipp Reinking <philipp@deck9.co>
2021-09-25 02:57:35 +0200

    output js files into dist/src

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    publicDir: false,
-    build: {
-        lib: {
-            entry: 'src/index.ts',
-            name: 'ui',
-            formats: ['es', 'umd'],
-            fileName: (format) => `index.${format}.js`,
-        },
-        rollupOptions: {
-            external: [
-                'vue',
-                '@fortawesome/fontawesome-svg-core',
-                '@fortawesome/free-solid-svg-icons',
-                '@fortawesome/vue-fontawesome'
-            ],
-            output: {
-                globals: {
-                    vue: 'Vue'
-                }
-            }
-        },
+  publicDir: false,
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "ui",
+      formats: ["es", "umd"],
+      fileName: (format) => `src/index.${format}.js`,
     },
-    plugins: [vue(), dts()]
-})
+    rollupOptions: {
+      external: [
+        "vue",
+        "@fortawesome/fontawesome-svg-core",
+        "@fortawesome/free-solid-svg-icons",
+        "@fortawesome/vue-fontawesome",
+      ],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  plugins: [vue(), dts()],
+});
