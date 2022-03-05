e48ba17fdcad5799b1c0f11bff443c5c17e0fe00
Philipp Reinking <philipp@deck9.co>
2022-03-05 17:11:54 +0100

    make spinner simple

--- src/Spinner/Spinner.vue
+++ src/Spinner/Spinner.vue
@@ -1,49 +1,7 @@
 <template>
-  <div class="grid grid-cols-3 grid-rows-3 gap-1 w-12 h-12">
-    <!-- 1 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.2s"
-    ></div>
-    <!-- 2 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.3s"
-    ></div>
-    <!-- 3 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.4s"
-    ></div>
-    <!-- 4 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.1s"
-    ></div>
-    <!-- 5 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.2s"
-    ></div>
-    <!-- 6 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.3s"
-    ></div>
-    <!-- 7 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0s"
-    ></div>
-    <!-- 8 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.1s"
-    ></div>
-    <!-- 9 -->
-    <div
-      class="animate-spinner bg-current rounded"
-      style="animation-delay: 0.2s"
-    ></div>
-  </div>
+  <D9Icon class="animate-spin" name="circle-notch" />
 </template>
+
+<script lang="ts" setup>
+import { D9Icon } from "../index";
+</script>
