57318cc780909fe485fa653438f23a373a68f0f5
Philipp Reinking <philipp@deck9.co>
2023-05-29 10:30:27 +0200

    fix bug with menu item double trigger

--- src/Menu/MenuLink.vue
+++ src/Menu/MenuLink.vue
@@ -1,18 +1,33 @@
 <template>
   <MenuItem v-slot="{ active }" v-bind="{ disabled }">
-    <span
-      class="flex items-center justify-between px-4 py-2 text-xs leading-5 transition-colors duration-100 rounded-lg"
+    <component
+      :is="as || 'span'"
+      class="
+        flex
+        items-center
+        justify-between
+        px-4
+        py-2
+        text-xs
+        leading-5
+        transition-colors
+        duration-100
+        rounded-lg
+      "
       :class="[
         active ? 'bg-grey-200' : '',
         disabled
           ? 'opacity-50 pointer-events-none'
           : 'hover:bg-grey-200 focus:bg-grey-200 focus:outline-none',
       ]"
-      v-bind="$attrs"
     >
       <span>{{ label }}</span>
-      <span v-if="meta" class="bg-grey-300 px-2 rounded-lg max-w-[66px] truncate">{{ meta }}</span>
-    </span>
+      <span
+        v-if="meta"
+        class="bg-grey-300 px-2 rounded-lg max-w-[66px] truncate"
+        >{{ meta }}</span
+      >
+    </component>
   </MenuItem>
 </template>
 
@@ -23,5 +38,6 @@ defineProps<{
   label?: string;
   meta?: string;
   disabled?: boolean;
+  as?: string;
 }>();
 </script>
