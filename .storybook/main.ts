afcf2fa502cacac95aa8530c7599ff05dfcd695c
Philipp Reinking <philipp@deck9.co>
2023-11-13 09:32:11 +0200

    update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,38 +1,43 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
     {
-      name: '@storybook/addon-postcss',
+      name: "@storybook/addon-styling-webpack",
       options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
+        postCss: {
+          implementation: require("postcss"),
         },
       },
     },
     "storybook-dark-mode",
   ],
-  "core": {
-    "builder": "webpack5"
-  },
 
   webpackFinal: async (config) => {
-
     config.module.rules.push({
       test: /\.less$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
-      include: path.resolve(__dirname, '../'),
+      use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+      include: path.resolve(__dirname, "../"),
     });
 
-    config.resolve.alias["@"] = path.resolve(__dirname, '../src');
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
 
     return config;
   },
-}
+
+  docs: {
+    autodocs: true,
+  },
+
+  framework: {
+    name: "@storybook/vue3-webpack5",
+    options: {},
+  },
+};
