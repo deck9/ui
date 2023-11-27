f0863fd332fff2aa12f9aa888f116020e507d000
Philipp Reinking <philipp@deck9.co>
2023-11-27 12:40:59 +0200

    fix output bundle

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: false,
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
   build: {
     lib: {
       entry: "src/index.ts",
@@ -16,6 +19,8 @@ export default defineConfig({
         "vue",
         "@fortawesome/fontawesome-svg-core",
         "@fortawesome/free-solid-svg-icons",
+        "@fortawesome/free-brands-svg-icons",
+        "@fortawesome/free-regular-svg-icons",
         "@fortawesome/vue-fontawesome",
       ],
       output: {
